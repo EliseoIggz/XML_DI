5021b233641b4285b2aaba056c2fe4dc32f70f43
EliseoIggz <eliseoiggz@gmail.com>
2025-02-03 20:23:05 +0100

    asdfcadws

--- PracticaFXML/test/com/example/practica3/practicafxml/model/RecordManagerTest.java
+++ PracticaFXML/test/com/example/practica3/practicafxml/model/RecordManagerTest.java
@@ -1,30 +1,62 @@
 package com.example.practica3.practicafxml.model;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class RecordManagerTest {
     private String url = "jdbc:sqlite:data/bbddPrueba.db";
-    private String sqlInsert = "INSERT INTO USER (nombre, tiempo) VALUES (?, ?)";
-    private String query = "SELECT nombre, tiempo FROM USER ";
+
     private Connection conn;
 
-
-    private List<Record> records;
+    private RecordManager recordManager;
 
     @BeforeEach
-    public Connection conectarBD() throws SQLException {
-        Connection connection = DriverManager.getConnection(url);
-        return connection;
+    public void conectarBD() throws SQLException {
+        conn  = DriverManager.getConnection(url);
+        recordManager = new RecordManager();
+    }
+
+    @AfterEach
+    public void cerrarBD() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
+    public void testGuardarRecords() {
+        Record record = new Record("Juan", 15);
+        boolean insercionExitosa = recordManager.guardarRecords(record);
 
+        if (insercionExitosa) {
+            System.out.println("El record fue insertado correctamente.");
+        } else {
+            System.out.println("Error al insertar el record.");
+        }
+    }
 
+    @Test
+    public void testCargarRecords() {
+        boolean cargaExitosa = recordManager.cargarRecords();
+
+        if (cargaExitosa) {
+            System.out.println("Los records se cargaron correctamente desde la base de datos.");
+        } else {
+            System.out.println("Hubo un error al cargar los records.");
+        }
+
+        if (!recordManager.getRecords().isEmpty()) {
+            System.out.println("Se cargaron " + recordManager.getRecords().size() + " records.");
+        } else {
+            System.out.println("No se cargaron records.");
+        }
+    }
 }
